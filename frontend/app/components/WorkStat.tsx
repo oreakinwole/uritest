f2a2bbdb3a82c2b7763e77438cc8a10d3dd42a51
oreakinwole <oreakinwole@gmail.com>
2024-11-13 23:55:43 +0100

    feat: sort and filterby logic

--- frontend/app/components/WorkStat.tsx
+++ frontend/app/components/WorkStat.tsx
@@ -1,120 +1,88 @@
 "use client";
 // import Image from "next/image";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { useState } from "react";
 import { Doughnut } from "react-chartjs-2";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-const dataSet = [
-  {
-    id: "01",
-    jobTitle: "DevOps Engineer",
-    companyName: "FusionTech",
-    status: "accepted",
-    dateApplied: "2024-08-25",
-  },
-  {
-    id: "02",
-    jobTitle: "Backend Developer",
-    companyName: "FusionTech",
-    status: "rejected",
-    dateApplied: "2024-07-18",
-  },
-  {
-    id: "03",
-    jobTitle: "Project Manager",
-    companyName: "MindForge",
-    status: "rejected",
-    dateApplied: "2024-06-04",
-  },
-  {
-    id: "04",
-    jobTitle: "Systems Analyst",
-    companyName: "DesignHut",
-    status: "accepted",
-    dateApplied: "2024-05-22",
-  },
-  {
-    id: "05",
-    jobTitle: "Software Engineer",
-    companyName: "UrbanCoders",
-    status: "accepted",
-    dateApplied: "2024-09-04",
-  },
-  {
-    id: "06",
-    jobTitle: "Data Scientist",
-    companyName: "UrbanCoders",
-    status: "rejected",
-    dateApplied: "2024-06-03",
-  },
-  {
-    id: "07",
-    jobTitle: "Full Stack Developer",
-    companyName: "UrbanCoders",
-    status: "pending",
-    dateApplied: "2024-05-05",
-  },
-  {
-    id: "08",
-    jobTitle: "Mobile Developer",
-    companyName: "UrbanCoders",
-    status: "rejected",
-    dateApplied: "2024-07-26",
-  },
-  {
-    id: "09",
-    jobTitle: "Software Engineer",
-    companyName: "ByteWorks",
-    status: "rejected",
-    dateApplied: "2024-07-12",
-  },
-  {
-    id: "10",
-    jobTitle: "Project Manager",
-    companyName: "MindForge",
-    status: "accepted",
-    dateApplied: "2024-08-29",
-  },
-  {
-    id: "11",
-    jobTitle: "Data Scientist",
-    companyName: "Skyline Solutions",
-    status: "accepted",
-    dateApplied: "2024-03-22",
-  },
-  {
-    id: "12",
-    jobTitle: "QA Engineer",
-    companyName: "NextGen",
-    status: "accepted",
-    dateApplied: "2024-01-03",
-  },
-];
-
-export const data = {
-  labels: ["Pending", "Rejected", "Accepted"],
-  datasets: [
-    {
-      label: "Statistics",
-      data: [12, 19, 3],
-      backgroundColor: ["yellow", "red", "green"],
-    },
-  ],
-};
+interface Application {
+  id: string;
+  jobTitle: string;
+  companyName: string;
+  status: string;
+  dateApplied: string;
+}
 
 export default function WorkStat() {
-  const filterBy = () => {};
+  const [filteredSet, setFilteredSet] = useState<undefined | Application[]>(
+    undefined
+  );
+
+  const [dataSet, setDataSet] = useState<undefined | Application[]>(undefined);
+
+  const [dataStats, setDataStats] = useState({
+    labels: ["Pending", "Rejected", "Accepted"],
+    datasets: [
+      {
+        label: "Statistics",
+
+        // Initial dummy data
+        data: [12, 19, 3],
+        backgroundColor: ["yellow", "red", "green"],
+      },
+    ],
+  });
+
+  const filterBy = (type: string) => {
+    if (type === "") setFilteredSet(undefined);
+    else if (dataSet) {
+      const sett = dataSet.filter((val) => val.status === type);
+      setFilteredSet(sett);
+    }
+  };
+
+  const toggleDateSort = (direction: string) => {
+    if (dataSet) {
+      const copyArr = [...dataSet];
+
+      if (direction === "" || direction === "ascending") {
+        copyArr.sort(
+          (a, b) =>
+            new Date(b.dateApplied).valueOf() -
+            new Date(a.dateApplied).valueOf()
+        );
+      }
+
+      if (direction === "descending") {
+        copyArr.sort(
+          (a, b) =>
+            new Date(a.dateApplied).valueOf() -
+            new Date(b.dateApplied).valueOf()
+        );
+      }
+
+      setDataSet(copyArr);
+    }
+  };
 
   return (
     <div className="p-28">
-      <header className="mb-10 pl-10">
-        <select>
+      <header className="mb-10 pl-10 max-w-[400px] flex justify-between">
+        <select onChange={(e) => filterBy(e.target.value)}>
           <option value="">Filter by</option>
-          <option value="status">Status</option>
-          <option value="date">Date</option>
+          <option value="pending">Pending</option>
+          <option value="accepted">Accepted</option>
+          <option value="rejected">Rejected</option>
+        </select>
+
+        <select onChange={(e) => toggleDateSort(e.target.value)}>
+          <option value="">Sort</option>
+          <option value="ascending">Latest</option>
+          <option value="descending">Oldest</option>
         </select>
       </header>
+
       <div className="flex  justify-between">
         <table className="text-center">
           <thead>
@@ -127,20 +95,32 @@ export default function WorkStat() {
           </thead>
 
           <tbody>
-            {dataSet.map((val) => (
-              <tr key={val.id}>
-                <td>{val.jobTitle}</td>
-                <td>{val.companyName}</td>
-                <td>{val.status}</td>
-                <td>{val.dateApplied}</td>
-              </tr>
-            ))}
+            {!filteredSet &&
+              dataSet &&
+              dataSet.map((val) => (
+                <tr key={val.id}>
+                  <td>{val.jobTitle}</td>
+                  <td>{val.companyName}</td>
+                  <td>{val.status}</td>
+                  <td>{val.dateApplied}</td>
+                </tr>
+              ))}
+
+            {filteredSet &&
+              filteredSet.map((val) => (
+                <tr key={val.id}>
+                  <td>{val.jobTitle}</td>
+                  <td>{val.companyName}</td>
+                  <td>{val.status}</td>
+                  <td>{val.dateApplied}</td>
+                </tr>
+              ))}
           </tbody>
         </table>
 
         <aside className="text-center">
           <h3>Statistics</h3>
-          <Doughnut data={data} />
+          <Doughnut data={dataStats} />
         </aside>
       </div>
     </div>
